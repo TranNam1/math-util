6de57eb316406b23cebd29cbf68cdcd2fc627211
TranNam1 <tranphuongnam99nct@gmail.com>
2020-11-19 08:35:13 +0700

    use recursion and GREEN again

--- src/namtp/util/MathUtil.java
+++ src/namtp/util/MathUtil.java
@@ -16,7 +16,21 @@ public class MathUtil {
     //ham tinh n giai thua n>-= ,111
     //neu qua 20 tran kieu long
     //0! = 1, 1! = 1, n!=1.2.3...n
-    public static long getFactorial(int n){
+//    public static long getFactorial(int n){
+//        if (n < 0 || n > 20) 
+//            throw  new IllegalArgumentException("n must be greater than 0 and less than 20");
+//        //dua dau vao ca chiwn , minh khong them tra ve gias tri am chinsai rooi
+//        
+//        if ( n == 0 || n == 1) {
+//            return 1;//0! = 1! = 1
+//        }
+//        long result = 1;
+//        for (int i = 2; i <= n; i++) {
+//            result = result * i;
+//        }
+//        return result;
+//    }
+     public static long getFactorial(int n){
         if (n < 0 || n > 20) 
             throw  new IllegalArgumentException("n must be greater than 0 and less than 20");
         //dua dau vao ca chiwn , minh khong them tra ve gias tri am chinsai rooi
@@ -24,11 +38,10 @@ public class MathUtil {
         if ( n == 0 || n == 1) {
             return 1;//0! = 1! = 1
         }
-        long result = 1;
-        for (int i = 2; i <= n; i++) {
-            result = result * i;
-        }
-        return result;
+        return n * getFactorial(n - 1);
+        //goi lai chinh miinh voi quy mo nho hon
+        //n! = n * (n-1)!
+        
     }
     
 }
